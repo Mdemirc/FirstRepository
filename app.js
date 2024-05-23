4e54c9dc934cd4b7ad3d8b470d1f5a744a57d746
Mdemirc <muhammedemindemirci13@gmail.com>
2024-05-23 19:47:18 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let value;
+
+value = document
+value = Document.all
+
+const elements = Document.all;
+for(let İ = 0; i <elements.lenght;i++) {
+    console.log(elements[i]);
+}
+
+
+
+// console.log(value)
